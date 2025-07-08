63e0d773eaa99e6d6b39e940ea04cfbb68690b78
Davi Reis <dreisss.dev@gmail.com>
2025-07-07 23:21:14 -0300

    feat(server): add and setup postgres with drizzle-orm

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -4,6 +4,7 @@ import { Type } from '@sinclair/typebox';
 import { Value } from '@sinclair/typebox/value';
 
 const envSchema = Type.Object({
+  DATABASE_URL: Type.String({ minLength: 1 }),
   SERVER_PORT: Type.Number({ default: '3000' }),
   WEB_PORT: Type.Number({ default: '3001' }),
 });
